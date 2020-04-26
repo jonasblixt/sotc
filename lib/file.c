6f723f9034c60d9c1fee130a1a8eefc2fd2e6a60
Jonas Blixt <jonpe960@gmail.com>
2020-04-26 12:04:56 +0200

    wip

--- lib/file.c
+++ lib/file.c
@@ -10,3 +10,14 @@ int sb_write_to_file(struct sb_root **root, const char *filename)
 {
     return 0;
 }
+
+
+/*
+ * The Incredible Machine (TIM)
+ *
+ * Marvellous Machine Maker (MMM)
+ *
+ * The Credible Machine (TCM)
+ *
+ *
+ */
