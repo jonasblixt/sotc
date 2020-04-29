92eb765e461059b4f8b466f043aa32bb3493f6c9
Jonas Blixt <jonpe960@gmail.com>
2020-04-29 07:19:18 +0200

    Add more comprehensive model test

--- test/test_model.c
+++ test/test_model.c
@@ -9,7 +9,6 @@ TEST(load_model)
 {
     int rc;
     struct tcm_model *model;
-    tcm_set_debug(tcm_test_debug);
 
     rc = tcm_model_load(TCM_TEST_SRC"test1.tcm", &model);
     ASSERT_EQ(rc, TCM_OK);
@@ -32,11 +31,30 @@ TEST(create_model)
     int rc;
     struct tcm_model *model;
 
-    tcm_set_debug(tcm_test_debug);
     rc = tcm_model_create(&model, "Test");
     ASSERT_EQ(rc, TCM_OK);
     ASSERT(model != NULL);
     ASSERT_EQ((char *) tcm_model_name(model), "Test");
+    rc = tcm_model_free(model);
+    ASSERT_EQ(rc, TCM_OK);
+}
+
+TEST(load_model2)
+{
+    int rc;
+    struct tcm_model *model;
+
+    rc = tcm_model_load(TCM_TEST_SRC"test2.tcm", &model);
+    ASSERT_EQ(rc, TCM_OK);
+
+    printf("Model load %i\n", rc);
+
+    json_object_object_foreach(model->jroot, key, val)
+    {
+        printf("%s: %s\n", key, json_object_get_string(val));
+    }
+
+
     rc = tcm_model_free(model);
     ASSERT_EQ(rc, TCM_OK);
 }
